ac79177024622742fd3ce4b262e1f566382f68d2
root <root@pcom-vm>
2024-11-04 01:17:06 +0200

    flooding still not working. Check line 117 from switch.py for details.

--- helpers.py
+++ helpers.py
@@ -1,6 +1,5 @@
-
-
-
+import os
+from switch import create_vlan_tag
 
 mac_table = {}
 
@@ -10,3 +9,48 @@ def update_MAC_table(mac_addr, interface):
         # add the new MAC address to the table if it doesn t exit 
         # or if it is on a different interface
         mac_table[mac_addr] = interface
+
+def add_vlan(vlan_config, vlan_id, interface):
+    if interface not in vlan_config:
+        vlan_config[interface] = []
+    if vlan_id not in vlan_config[interface]:
+        vlan_config[interface].append(vlan_id)
+
+def load_vlan_config(switch_id):
+    vlan_config = {}
+    switch_priorities = {}
+    config_dir = 'configs'
+    
+    for filename in os.listdir(config_dir):
+        if filename.startswith('switch') and filename.endswith('.cfg') and filename[6] == switch_id:
+            try:
+                with open(os.path.join(config_dir, filename), 'r') as f:
+                    lines = f.readlines()
+                    switch_priorities[switch_id] = int(lines[0])
+                    
+                    for line in lines[1:]:  # Skip the first line (priority)
+                        if line[0] == 'r' and line[1] == '-':
+                            # format interface - vlan ID
+                            interface, vlan_id = line.strip().split(' ')
+                            vlan_id = int(vlan_id)
+                            add_vlan(vlan_config, vlan_id, interface)
+                        elif ' T' in line:
+                            # we have a trunk
+                            trunk_interface, _ = line.strip().split(' ')
+                            # Add trunk interface to all VLANs
+                            vlan_id = -1
+                            add_vlan(vlan_config, vlan_id, trunk_interface)
+                            
+            except ValueError as e:
+                print(f"Error processing file {filename}: {e}")
+            break
+    
+    return vlan_config, switch_priorities
+
+def add_8021Q_header(data, vlan_id):
+    # Add a VLAN tag to the frame
+    return data[0:12] + create_vlan_tag(vlan_id) + data[12:]
+
+def remove_8021Q_header(data):
+    # Remove the VLAN tag from the frame
+    return data[0:12] + data[16:]
--- switch.py
+++ switch.py
@@ -54,7 +54,9 @@ def main():
     # Printing interface names
     for i in interfaces:
         print(get_interface_name(i))
+    
 
+    vlan_config, switch_priority = load_vlan_config(switch_id)
     while True:
         # Note that data is of type bytes([...]).
         # b1 = bytes([72, 101, 108, 108, 111])  # "Hello"
@@ -67,10 +69,7 @@ def main():
         # Print the MAC src and MAC dst in human readable format
         dest_mac = ':'.join(f'{b:02x}' for b in dest_mac)
         src_mac = ':'.join(f'{b:02x}' for b in src_mac)
-
-        # Note. Adding a VLAN tag can be as easy as
-        # tagged_frame = data[0:12] + create_vlan_tag(10) + data[12:]
-
+        
         print(f'Destination MAC: {dest_mac}')
         print(f'Source MAC: {src_mac}')
         print(f'EtherType: {ethertype}')
@@ -80,15 +79,60 @@ def main():
         # TODO: Implement forwarding with learning
         update_MAC_table(src_mac, interface)
         
+        
+        interface_name = get_interface_name(interface)
+        # Implement VLAN support
+        if vlan_id == -1:
+            # check vlan_config for the vlan_id
+            for i in vlan_config:
+                if interface_name == i:
+                    vlan_id = vlan_config[i][0]
+        
+        print (f"VLAN ID: {vlan_id}")
+        # Forward the frame based on the destination MAC address
         if dest_mac in mac_table:
             # Forward the frame to the interface where the destination MAC is located
-            send_to_link(mac_table[dest_mac], length, data)
+            dest_interface = mac_table[dest_mac]
+            if dest_interface != interface:
+                dest_interface_name = get_interface_name(dest_interface)
+                if dest_interface_name in vlan_config:
+                    # check if interface is trunk or access
+                    if vlan_config[dest_interface_name] == -1:
+                        # Add 802.1Q header if forwarding to a trunk port
+                        # check first if data already has a VLAN tag
+                        if data[12] != 0x81 and data[13] != 0x00:
+                            data = add_8021Q_header(data, vlan_id)
+                    else:
+                        # Remove 802.1Q header if forwarding to an access port
+                        # check first if data already has a VLAN tag
+                        if data[12] == 0x81 and data[13] == 0x00:
+                            data = remove_8021Q_header(data)
+                    send_to_link(dest_interface, len(data), data)
+                else:
+                    print(f"Destination interface {dest_interface} not in VLAN {vlan_id}. Dropping frame.")
         else:
-            # Flood the frame to all interfaces except the one where it came from
-            for i in interfaces:
-                if i != interface:
-                    send_to_link(i, length, data)
-        # TODO: Implement VLAN support
+            # Flood the frame to all interfaces except the one where it came from, keeping in mind the VLAN
+            
+            #########################33
+            # make the for from 1 to interface number max and pick only the ones in the vlan_config
+            # so u have access to both the nr and the name of the interface - for the send_link function
+            ########################3##
+            for i in vlan_config.keys():
+                if i != interface_name:
+                    if vlan_id == -1:
+                        # Add 802.1Q header if forwarding to a trunk port
+                        # check first if data already has a VLAN tag
+                        if data[12] != 0x81 and data[13] != 0x00:
+                            data = add_8021Q_header(data, vlan_id)
+                    else:
+                        # Remove 802.1Q header if forwarding to an access port
+                        # check first if data already has a VLAN tag
+                        if data[12] == 0x81 and data[13] == 0x00:
+                            data = remove_8021Q_header(data)
+                    print(f"Flooding frame to interface {i}")
+                    send_to_link(get_interface(i), len(data), data)
+                else:
+                    print(f"Interface {i} not in VLAN {vlan_id}. Dropping frame.")
         
         # TODO: Implement STP support
 
